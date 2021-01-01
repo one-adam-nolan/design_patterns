2142939900a3305ff74ec1647d93727ee2b8535f
Adam Nolan <anolan@apterainc.com>
2021-01-01 14:41:17 -0500

    feat: add projects, stylecopy, create first absract

---
+++ src/PizzaStore.Pizzas/Pizzas/Abstract/Pizza.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace PizzaStore.Domain.Pizzas.Abstract
+{
+    public abstract class Pizza
+    {
+        public Pizza()
+        {
+        }
+
+        public void Prepare()
+        {
+            Console.WriteLine("Preparing the Pizza");
+        }
+
+        public void Bake()
+        {
+            Console.WriteLine("Baking the Pizza");
+        }
+
+        public void Cut()
+        {
+            Console.WriteLine("Cutting the Pizza");
+        }
+
+        public void Box()
+        {
+            Console.WriteLine("Putting the za' in the box");
+        }
+    }
+}
